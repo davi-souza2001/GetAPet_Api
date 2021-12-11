3a7916dd8e448165790358ccd7ebef70849f44c4
davi-souza2001 <davisouza2001dv@gmail.com>
2021-12-11 20:31:09 -0300

    save user at database

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -9,41 +10,60 @@ module.exports = class UserController {
         const confirmpassword = req.body.confirmpassword
 
         //validations
-        if(!name) {
-            res.status(422).json({ message: 'O nome é obrigatório'})
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
             return
         }
-        if(!email) {
-            res.status(422).json({ message: 'O nome é email'})
+        if (!email) {
+            res.status(422).json({ message: 'O nome é email' })
             return
         }
-        if(!phone) {
-            res.status(422).json({ message: 'O telefone é obrigatório'})
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório' })
             return
         }
-        if(!password) {
-            res.status(422).json({ message: 'A senha é obrigatória'})
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
             return
         }
-        if(!confirmpassword) {
-            res.status(422).json({ message: 'A confirmação de senha é obrigatória'})
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
             return
         }
 
-        if(password !== confirmpassword){
-            res.status(422).json({message: "A confirmação de senha precisa ser igual à senha !"})
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: "A confirmação de senha precisa ser igual à senha !" })
             return
         }
 
         //check if user exists
-        const userExists = await User.findOne({email: email})
+        const userExists = await User.findOne({ email: email })
 
-        if(userExists){
-            res.status(422).json({message: "Email já cadastrado !"})
+        if (userExists) {
+            res.status(422).json({ message: "Email já cadastrado !" })
             return
         }
 
+        // create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
 
-        res.send({message: "Todos os dados passaram"})
+        // create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
+
+        try {
+            const newUser = await user.save()
+            res.status(201).json({
+                message: 'Usuário criado !',
+                newUser
+            })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
     }
 }

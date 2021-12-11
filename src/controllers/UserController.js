8e221ccef2ea784e414b103acafcb35b1b303c7f
davi-souza2001 <davisouza2001dv@gmail.com>
2021-12-11 19:10:57 -0300

    add credencials check user exists and all params exists

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,6 +2,48 @@ const User = require('../models/User')
 
 module.exports = class UserController {
     static async register(req, res) {
-        res.json('Ola Get a Pet')
+        const name = req.body.name
+        const email = req.body.email
+        const phone = req.body.phone
+        const password = req.body.password
+        const confirmpassword = req.body.confirmpassword
+
+        //validations
+        if(!name) {
+            res.status(422).json({ message: 'O nome é obrigatório'})
+            return
+        }
+        if(!email) {
+            res.status(422).json({ message: 'O nome é email'})
+            return
+        }
+        if(!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório'})
+            return
+        }
+        if(!password) {
+            res.status(422).json({ message: 'A senha é obrigatória'})
+            return
+        }
+        if(!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: "A confirmação de senha precisa ser igual à senha !"})
+            return
+        }
+
+        //check if user exists
+        const userExists = await User.findOne({email: email})
+
+        if(userExists){
+            res.status(422).json({message: "Email já cadastrado !"})
+            return
+        }
+
+
+        res.send({message: "Todos os dados passaram"})
     }
 }

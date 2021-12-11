a175a2067bea7890baa046a59a73fad5d4d2cde0
davi-souza2001 <davisouza2001dv@gmail.com>
2021-12-11 20:56:34 -0300

    finish check credencials user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -16,7 +16,7 @@ module.exports = class UserController {
             return
         }
         if (!email) {
-            res.status(422).json({ message: 'O nome é email' })
+            res.status(422).json({ message: 'O email é obrigatório' })
             return
         }
         if (!phone) {
@@ -63,5 +63,37 @@ module.exports = class UserController {
         } catch (error) {
             res.status(500).json({ message: error })
         }
+    }
+
+    static async login(req, res) {
+        const {email, password} = req.body
+
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório' })
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+
+        //check if user exists
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            res.status(422).json({ message: "Não há cadastrado com esse email !" })
+            return
+        }
+
+        //check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword) {
+            res.status(422).json({ message: "Senha inválida !" })
+            return
+        }
+
+        await createUserToken(user, req, res)
     }
 }
--- src/helpers/create-user-token.js
+++ src/helpers/create-user-token.js
@@ -12,7 +12,7 @@ const createUserToken = async (user, req, res) => {
     res.status(200).json({
         message: 'Você está autenticado',
         token: token,
-        userId: user._id
+        user
     })
 }
 

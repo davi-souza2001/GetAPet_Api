f329a3f6929e3933d9b5c645aa52062d4018227c
davi-souza2001 <davisouza2001dv@gmail.com>
2021-12-12 15:53:28 -0300

    create a new pet finish

--- src/routes/PetRoutes.js
+++ src/routes/PetRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const Petcontroller = require('../controllers/Petcontroller')
 
-router.post('/create', Petcontroller.create)
+// middlewares
+const verifytoken = require('../helpers/verify-token')
+
+router.post('/create', verifytoken, Petcontroller.create)
 
 module.exports = router
